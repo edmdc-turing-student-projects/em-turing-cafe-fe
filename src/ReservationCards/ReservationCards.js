85c4a04cf9a4cfba89992663ab6b3f60f2c3bc82
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-07-16 11:29:23 -0600

    "Create card component for more granular test"

---
+++ src/ReservationCards/ReservationCards.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './ReservationCards.css'
+
+const ReservationCards = ({ reservations }) => {
+  console.log(reservations)
+  const renderReservations = () => {
+    return reservations.map((reservation, index) => {
+      return (
+        <article className='single-resy' key={index}>
+          <h4>{reservation.name}</h4>
+          <p>{reservation.date}</p>
+          <p>{reservation.time}</p>
+          <p>{`Guest Number:${reservation.number}`}</p>
+        </article>
+      )
+    })
+  }
+
+  return (
+    <>
+      {renderReservations()}
+    </>
+      )
+};
+
+export default ReservationCards;
